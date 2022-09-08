38ac9d03f4bf087209262b141db2dca782358025
Jacob Wright <jacwright@gmail.com>
2022-09-07 21:53:08 -0600

    Make property names clearer

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
-export declare function inc(str: string, pad?: number): string;
+export declare function inc(counter: string, pad?: number): string;
 export declare namespace inc {
     var is: (a: string) => {
         lt: (b: string) => boolean;
         gt: (b: string) => boolean;
         eq: (b: string) => boolean;
     };
-    var from: (n: number, pad?: number) => string;
-    var to: (str: string, pad?: number) => number;
+    var from: (number: number, pad?: number) => string;
+    var to: (counter: string, pad?: number) => number;
 }
